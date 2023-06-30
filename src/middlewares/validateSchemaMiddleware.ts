db90570fcd040ef28a3aa8313a3da4f6d6a6adcf
Vinicius <vinicezar800@hotmail.com>
2023-06-30 09:10:43 -0300

    feat: implement schema validation function

---
+++ src/middlewares/validateSchemaMiddleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express'
+import { ObjectSchema } from 'joi'
+
+export default function validateSchema(schema: ObjectSchema) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const { body } = req
+    const { error } = schema.validate(body, { abortEarly: true })
+
+    if (error) {
+      return res.status(400).send(error.message)
+    }
+
+    next()
+  }
+}
